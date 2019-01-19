89d9cf64b62194fa3e4e1a034b3a3f705bd47ba8
dantal2000 <dantal2000@yandex.ru>
2019-01-19 21:35:55 +0300

    Put off log messages

--- bin/download.pl
+++ bin/download.pl
@@ -10,7 +10,7 @@ if (@ARGV != 1) {
 };
 
 $track_id = $ARGV[0];
-print "track_id = $track_id\n";
+#print "track_id = $track_id\n";
 
 # id is a number
 if (! ($track_id =~ /^\d+\z/)) {
@@ -24,18 +24,18 @@ if (! ($track_id =~ /^\d+\z/)) {
 $quality = 0;
 
 $first_URL = "https://music.yandex.ru/api/v2.1/handlers/track/$track_id/track/download/m?hq=$quality";
-print "first_url = $first_URL\n";
+#print "first_url = $first_URL\n";
 $first_json = `curl --url $first_URL --header "X-Retpath-Y: https://music.yandex.ru/"`;
-print "first_json = $first_json\n";
+#print "first_json = $first_json\n";
 
 $second_URL = (JSON->new->utf8->decode($first_json))->{src};
 
 @second_get_data = ("format=json","external-domain=music.yandex.ru","overembed=no",("__t=" . time));
 $second_URL .= "&$_" foreach @second_get_data;
-print "secon_url = $second_URL";
+#print "secon_url = $second_URL";
 
 $second_json_string = `curl --url "$second_URL"`;
-print "second_json_string = $second_json_string";
+#print "second_json_string = $second_json_string";
 
 $second_json = JSON->new->utf8->decode($second_json_string);
 $s = $second_json->{s};
@@ -43,19 +43,19 @@ $ts = $second_json->{ts};
 $path = $second_json->{path};
 $host = $second_json->{host};
 
-print <<END_LIST;
-s = $s
-ts = $ts
-path = $path
-host = $host
-END_LIST
+#print <<END_LIST;
+#s = $s
+#ts = $ts
+#path = $path
+#host = $host
+#END_LIST
 
 $md5_salt = "XGRlBW9FXlekgbPrRHuSiA";
 $string_to_hash = $md5_salt . substr($path, 1) . $s;
-print "string_to_hash = $string_to_hash\n";
+#print "string_to_hash = $string_to_hash\n";
 $md5_hash = md5_hex($string_to_hash);
 
 $third_URL = "https://$host/get-mp3/$md5_hash/". $ts . $path . "?track-id=$track_id";
-print "third_url = $third_URL\n";
+#print "third_url = $third_URL\n";
 system("curl --url \"$third_URL\" -o $track_id.mp3");
 
